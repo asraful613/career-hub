3214237109b18315dda358c5ebaec78037227fde
asraful613 <atifasraful530@gmail.com>
2024-03-23 15:30:58 +0000

    added a banner image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,16 @@
 const Banner = () => {
     return (
         <div>
-            <h2 className="text-center text-5xl">Home page Banner</h2>
+           <div className="hero">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="/src/assets/images/user.png" className="max-w-sm rounded-lg shadow-xs" />
+        <div>
+      <h1 className="text-5xl font-bold">One Step Closer To Your <br></br><span className="text-blue-500"> Dream Job!</span></h1>
+      <p className="py-6">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. <br/> Manage all your job application from start to finish.</p>
+      <button className="btn text-white bg-slate-800">Get Started</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
